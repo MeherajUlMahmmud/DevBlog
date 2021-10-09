cd2a5596ccd7a7d7c86460f6e019e4ce4e719c96
Meheraj <meherajmahmmd@gmail.com>
2021-10-10 00:20:45 +0600

    Create constants.dart

---
+++ flutter_mobile/lib/utils/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  // ignore: non_constant_identifier_names
+  static String API = "http://192.168.0.108/api/";
+  static String BLOG_API = API + "blog/";
+  static String USER_API = API + "user/";
+
+  static String loginUrl = USER_API + "login/";
+  static String registerUrl = USER_API + "register/";
+}
