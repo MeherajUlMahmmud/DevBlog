b65a9b0ad242892b6c674e49d2487fc82da864b6
Meheraj <meherajmahmmd@gmail.com>
2021-10-10 00:23:18 +0600

    Create settings.dart

---
+++ flutter_mobile/lib/screens/util_screens/settings.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobile/screens/main_screen.dart';
+import 'package:flutter_mobile/screens/util_screens/change_password.dart';
+import 'package:flutter_mobile/screens/util_screens/feedback.dart';
+import 'package:flutter_mobile/utils/util_func.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  String _res;
+  bool isLoading = false;
+  bool isLoggedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getToken();
+  }
+
+  void getToken() async {
+    setState(() {
+      isLoading = true;
+    });
+    Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+    final SharedPreferences prefs = await _prefs;
+    setState(() {
+      _res = prefs.getString("token");
+    });
+    print(_res);
+
+    if (_res != null) {
+      setState(() {
+        isLoading = false;
+        isLoggedIn = true;
+      });
+    } else {
+      setState(() {
+        isLoading = false;
+        isLoggedIn = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.feedback),
+                    title: Text('Send Feedback'),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => FeedbackScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.info),
+                    title: Text('About'),
+                    onTap: () {
+                      // Navigator.of(context).push(
+                      //   MaterialPageRoute(
+                      //     builder: (context) => AboutScreen(),
+                      //   ),
+                      // );
+                    },
+                  ),
+                  isLoggedIn
+                      ? Column(
+                          children: [
+                            ListTile(
+                              // change password
+                              leading: Icon(Icons.lock),
+                              title: Text('Change Password'),
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => ChangePasswordScreen(),
+                                  ),
+                                );
+                              },
+                            ),
+                            ListTile(
+                              leading: Icon(Icons.exit_to_app),
+                              title: Text('Logout'),
+                              onTap: () {
+                                // show dialog
+                                showDialog(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                    title: Text('Logout'),
+                                    content: Text(
+                                        'Are you sure you want to logout?'),
+                                    actions: <Widget>[
+                                      TextButton(
+                                        child: Text('Yes'),
+                                        onPressed: () {
+                                          UtilityFunction.clearSharedPreferences(context);
+                                          Navigator.of(context).pop();
+                                          Navigator.of(context).pushReplacement(
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  MainScreen(),
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                      TextButton(
+                                        child: Text('No'),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        )
+                      : Container(),
+                ],
+              ),
+            ),
+    );
+  }
+}
