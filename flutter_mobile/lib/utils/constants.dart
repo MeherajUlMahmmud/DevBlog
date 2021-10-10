44a7e07d03eb9e3c1d30368e407261f81c1ce90d
Meheraj <meherajmahmmd@gmail.com>
2021-10-11 01:03:17 +0600

    Update constants.dart

--- flutter_mobile/lib/utils/constants.dart
+++ flutter_mobile/lib/utils/constants.dart
@@ -1,9 +1,40 @@
+import 'package:flutter/material.dart';
+
 class Constants {
-  // ignore: non_constant_identifier_names
   static String API = "http://192.168.0.108/api/";
   static String BLOG_API = API + "blog/";
   static String USER_API = API + "user/";
 
   static String loginUrl = USER_API + "login/";
   static String registerUrl = USER_API + "register/";
+
+
+  static Color lightPrimary = Colors.white;
+  static Color darkPrimary = Colors.black;
+  static Color lightAccent = Colors.black;
+  static Color darkAccent = Colors.red[400];
+  static Color lightBG = Color(0xfffcfcff);
+  static Color darkBG = Colors.black;
+
+  static ThemeData lightTheme = ThemeData(
+    backgroundColor: lightBG,
+    primaryColor: lightPrimary,
+    accentColor: lightAccent,
+    textSelectionTheme: TextSelectionThemeData(
+      cursorColor: lightAccent,
+    ),
+    scaffoldBackgroundColor: lightBG,
+    appBarTheme: AppBarTheme(
+      textTheme: TextTheme(
+        headline6: TextStyle(
+          color: darkBG,
+          fontSize: 18.0,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+//      iconTheme: IconThemeData(
+//        color: lightAccent,
+//      ),
+    ),
+  );
 }
