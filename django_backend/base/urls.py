fa1158a606796d5da401127b78b6dfd946289d2c
Meheraj <meherajmahmmd@gmail.com>
2021-10-10 18:16:11 +0600

    Update urls.py

--- django_backend/base/urls.py
+++ django_backend/base/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from user_control.views import get_routes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_routes),
     path('api/user/', include('user_control.urls')),
     path('api/blog/', include('blog_control.urls')),
 ]
