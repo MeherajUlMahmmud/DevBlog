228e7a66d8b1ea4d4b92cca47699bcff8d4c3c8f
Meheraj <meherajmahmmd@gmail.com>
2021-10-11 01:07:57 +0600

    Create deactivate_account.dart

---
+++ flutter_mobile/lib/screens/util_screens/deactivate_account.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DeactivateAccountScreen extends StatefulWidget {
+
+  @override
+  _DeactivateAccountScreenState createState() => _DeactivateAccountScreenState();
+}
+
+class _DeactivateAccountScreenState extends State<DeactivateAccountScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
