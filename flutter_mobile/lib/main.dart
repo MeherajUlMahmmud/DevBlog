71e965fbc879c00aaa991aadea17623d77c94720
Meheraj <meherajmahmmd@gmail.com>
2021-10-10 18:16:18 +0600

    Update main.dart

--- flutter_mobile/lib/main.dart
+++ flutter_mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobile/screens/main_screen.dart';
+import 'package:flutter_mobile/utils/constants.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      theme: Constants.lightTheme,
       home: MainScreen(),
     );
   }
