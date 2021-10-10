730632ca78d95917f054f75ecccf556addf4fa04
Meheraj <meherajmahmmd@gmail.com>
2021-10-10 18:16:48 +0600

    Update admin.py

--- django_backend/blog_control/admin.py
+++ django_backend/blog_control/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import PostModel, CommentModel
+from .models import BlogModel, CommentModel
 
-admin.site.register(PostModel)
+admin.site.register(BlogModel)
 admin.site.register(CommentModel)
