bc2ffe1e2b5bae273697bfef65c469fc649b7807
Meheraj <meherajmahmmd@gmail.com>
2021-10-12 00:47:14 +0600

    Update categories_screen.dart

--- flutter_mobile/lib/screens/blog_screens/categories_screen.dart
+++ flutter_mobile/lib/screens/blog_screens/categories_screen.dart
@@ -1,7 +1,8 @@
+import 'package:DevBlog/widgets/blog_card.dart';
+import 'package:DevBlog/widgets/category_tile.dart';
 import 'package:flutter/material.dart';
 
 class CategoriesScreen extends StatefulWidget {
-
   @override
   _CategoriesScreenState createState() => _CategoriesScreenState();
 }
@@ -10,8 +11,70 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Categories'),
+      // create a horizontal list view
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 5.0),
+        child: ListView(
+          children: [
+            Container(
+              height: 40.0,
+              margin: const EdgeInsets.symmetric(vertical: 10.0),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  CategoryTile(
+                    title: 'React.js',
+                  ),
+                  CategoryTile(
+                    title: 'Django',
+                  ),
+                  CategoryTile(
+                    title: 'AI',
+                  ),
+                  CategoryTile(
+                    title: 'Python',
+                  ),
+                  CategoryTile(
+                    title: 'Java',
+                  ),
+                  CategoryTile(
+                    title: 'Node.js',
+                  ),
+                  CategoryTile(
+                    title: 'Redis',
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height - 70,
+              child: ListView(
+                children: [
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                  BlogCard(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
