5716196105227249ab695448a48c7206dbacdf46
Meheraj <meherajmahmmd@gmail.com>
2021-10-18 20:14:04 +0600

    Update blog_card.dart

--- flutter_mobile/lib/widgets/blog_card.dart
+++ flutter_mobile/lib/widgets/blog_card.dart
@@ -1,3 +1,4 @@
+import 'package:DevBlog/screens/blog_screens/blog_details.dart';
 import 'package:flutter/material.dart';
 
 class BlogCard extends StatefulWidget {
@@ -21,60 +22,70 @@ class BlogCard extends StatefulWidget {
 class _BlogCardState extends State<BlogCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 10),
-      child: Card(
-        elevation: 5,
-        child: Container(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          'This is the blog title',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => BlogDetailsScreen(id: 1),
+          ),
+        );
+      },
+      child: Container(
+        margin: EdgeInsets.only(bottom: 10),
+        child: Card(
+          elevation: 5,
+          child: Container(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'This is the blog title',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.justify,
                           ),
-                          textAlign: TextAlign.justify,
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          'Author',
-                          style: TextStyle(
-                            fontSize: 15,
+                          SizedBox(height: 5),
+                          Text(
+                            'Author',
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(width: 10),
-                  Image.network(
-                    'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=100&q=60',
-                    height: 80,
-                    width: 100,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Text(
-                'Date Posted',
-                style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.grey,
+                    SizedBox(width: 10),
+                    Image.network(
+                      'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=100&q=60',
+                      height: 80,
+                      width: 100,
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  'Date Posted',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
